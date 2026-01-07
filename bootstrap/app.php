7025a0850e61eed9b7cd770cd796485d3dff1451
luizrhossard <100325342+luizrhossard@users.noreply.github.com>
2026-01-07 14:57:13 -0300

    Update middleware to trust additional proxy headers

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,7 +14,10 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->trustProxies(
             at: '*',
-            headers: Request::HEADER_X_FORWARDED_ALL
+            headers: Request::HEADER_X_FORWARDED_FOR
+                | Request::HEADER_X_FORWARDED_HOST
+                | Request::HEADER_X_FORWARDED_PORT
+                | Request::HEADER_X_FORWARDED_PROTO
         );
     })
     ->withExceptions(function (Exceptions $exceptions): void {
